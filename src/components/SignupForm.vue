22ce1ff687fab5380bd69311816dc75dcce2858c
MaryahKim <maryapitaz@gmail.com>
2022-02-03 10:40:41 +0300

    Update SignupForm.vue
    add labels and input fields  in form
    create methods for key up event listener
    add styles for labels and input

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,16 +1,56 @@
 <template>
   <form>
       <label>Email:</label>
-      <input type="email" required>
+      <input type="email" required v-model="email">
+
+      <label>Password:</label>
+      <input type="password" required v-model="password">
+
+      <label>Role</label>
+      <select v-model="role">
+          <option value="developer">Web Developer</option>
+          <option value="designer">Web Designer</option>
+      </select>
+
+      <div class="terms">
+          <input type="checkbox" required v-model="terms">
+          <label > Accept terms and conditions</label>
+      </div>
+
+      <label >Skills:</label>
+      <input type="text" v-model="tempSkills" @keyup="addskill">
   </form>
+
+  <p>Email: {{ email }}</p>
+    <p>Password: {{ password }}</p>
+    <p>Role: {{ role }}</p>
+
 </template>
 
 <script>
 export default {
-
+    data() {
+        return{
+            email: '',
+            password: '',
+            role: '',
+            terms:'false',
+            tempSkills:'',
+            skills: []
+        }
+    },
+   methods: {
+       addskill(e) {
+           if (e.key === ',' && this.tempSkills) {
+               this.skills.push(this.tempSkills)
+               this.tempSkills = ''
+           }
+       }
+   }
 }
 </script>
-
+    
+}
 <style>
 form {
     max-width: 420px;
@@ -25,7 +65,7 @@ label {
     color: #aaa;
     display: inline-block;
     margin: 25px 0 15px;
-    font-size: 0.6em;
+    font-size: 0.8em;
     text-decoration: uppercase;
     letter-spacing: 1px;
     font-weight: bold;
@@ -39,5 +79,20 @@ input {
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+}
+
+select{
+     width: 100%;
+     border: none;
+     border-bottom: 1px solid #ddd;
+     color: #555;
+}
+
+input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
 }
 </style>
